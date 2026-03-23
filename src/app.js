c8c5aebc6fd6d91ed84d18bc5120f55acb361fac
Aliyaan <smdaliyaan@gmail.com>
2026-03-23 22:12:26 +0530

    Implement API error handling and response classes; add async handler utility

--- src/app.js
+++ src/app.js
@@ -15,3 +15,5 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 export default app;
+
+// export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,35 @@
+//const asyncHandler = () => {} 
+/* asyncHandler is a higher-order function that takes an asynchronous function
+as an argument and returns a new function that wraps the original function
+in a try-catch block. This allows you to handle errors in a consistent way
+across your application.*/
+
+//const asyncHandler = () => {
+// const (func) => () => {}
+// const (func) => async () => {}
+
+    // try catch async Handler 
+
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
+
+    // approach using promise
+
+const asyncHandler = (requesthandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+export default asyncHandler;
+
+
