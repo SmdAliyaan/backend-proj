0ad2517b69c5c9090c4d2cc4cc795208f9348398
Aliyaan <smdaliyaan@gmail.com>
2026-03-26 18:31:44 +0530

    Add user registration route and controller

--- src/app.js
+++ src/app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export default app;
 
-// export { app }
+
+//routes import 
+
+import userRouter from './routes/user.routes.js'
+
+//routes declearation
+app.use("/api/v1/users",userRouter)
+
+
+export{ app }
