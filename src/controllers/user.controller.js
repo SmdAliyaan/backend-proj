722dcfdbc90ae5d40b62b6291cd4c7fb67caa56b
Aliyaan <smdaliyaan@gmail.com>
2026-04-03 22:45:13 +0530

    Implement refresh token functionality with error handling and update routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -186,16 +186,48 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
         throw new ApiError(401, "unauthorized request")
     }
 
-    const decodedToken = jwt.verify(
-        incomingRefreshToken,
-        process.env.REFRESH_TOKEN_SECRET
-    )
-
-    const user = await User.findById(decodedToken._id)
-
-    if(!user){
-        throw new ApiError(401,"Invalid refresh token")
-    }
+try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+    
+        const user = await User.findById(decodedToken._id)
+    
+        if(!user){
+            throw new ApiError(401,"Invalid refresh token")
+        }
+    
+    
+        if(incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401,"Refres token is expired or used")
+        }
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        const {accessToken,newRefreshToken}  = await
+        generateAccessAndRefreshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken, options)
+        .cookie("refreshToken",newRefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken,
+                    refreshToken : newRefreshToken
+                },
+                    "Access token refreshed"
+            )
+        )
+} catch (error) {
+    throw new ApiError(401,error?.message || "Invalid refresh token")
+}
 })
 
 
@@ -203,5 +235,6 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
 export { 
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
